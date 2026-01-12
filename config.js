7eb840632777e0b4f8d3ec7340a838e51095aec3
Yuyao Liu <liuyuyao21@mails.tsinghua.edu.cn>
2026-01-12 18:40:41 +0800

    init

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  GAS_URL: "https://script.google.com/macros/s/AKfycbzfpr8qoadMzyzTP628IgX1Ul8EZQvTL-Utvp0YgllpgqjpM55GEwG0NBhKGgQw61YQ/exec",
+  PROLIFIC_COMPLETION_URL: "https://app.prolific.com/submissions/complete?cc=C19DWP4Q",
+};
